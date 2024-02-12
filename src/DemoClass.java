5447e24d41938bd8ee2d5f6acdfc36c54dff7586
jpkaul123 <159743503+jpkaul123@users.noreply.github.com>
2024-02-12 13:05:29 +0530

    Update DemoClass.java

--- src/DemoClass.java
+++ src/DemoClass.java
@@ -4,6 +4,7 @@ public class DemoClass {
 	public static void main(String[] args) {
 	// TODO Auto-generated method stub
 WebDriver driver = new ChromeDriver();
+		driver.get("www.google.com");
 	}
 
 }
